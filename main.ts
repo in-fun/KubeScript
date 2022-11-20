37c18248829e4ddc9d099c6221c7882ee6e11e56
Junjun Deng <junjun@tubi.tv>
2022-11-20 23:53:05 +0800

    Fix file path

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read --allow-env
+#!/usr/bin/env -S deno run --unstable --allow-net --allow-read --allow-env
 
 import {yaml} from './mod.ts'
 // import objects from './example/nginx/mod.ts'
@@ -6,7 +6,7 @@ import {yaml} from './mod.ts'
 const path = Deno.realPathSync(Deno.args[0])
 const isDir = Deno.statSync(path).isDirectory
 const modPath = isDir ? `${path}/mod.ts` : path
-const module = await import(modPath)
+const module = await import(`file://${modPath}`)
 const objects = module.default
 for (const object of objects) {
     const s = yaml.stringify(object as Record<string, unknown>)
