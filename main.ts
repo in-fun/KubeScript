54474b8a995e4c13decf8801f15ce6556e3e0243
lilac <cnjamesdeng@gmail.com>
2022-11-24 22:58:29 +0800

    Accept url input

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 #!/usr/bin/env -S deno run --unstable --allow-net --allow-read --allow-env
 
-import {yaml} from './mod.ts'
-// import objects from './example/nginx/mod.ts'
+import { yaml } from "./mod.ts";
 
-const path = Deno.realPathSync(Deno.args[0])
-const isDir = Deno.statSync(path).isDirectory
-const modPath = isDir ? `${path}/mod.ts` : path
-const module = await import(`file://${modPath}`)
-const objects = module.default
+let url = Deno.args[0];
+if (url.search(":") < 0) {
+  // a local path
+  const path = Deno.realPathSync(url);
+  const isDir = Deno.statSync(path).isDirectory;
+  const modPath = isDir ? `${path}/mod.ts` : path;
+  url = `file://${modPath}`;
+}
+const module = await import(url);
+const objects = module.default;
 for (const object of objects) {
-    const s = yaml.stringify(object as Record<string, unknown>)
-    console.log(s)
-    console.log('---')
+  const s = yaml.stringify(object as Record<string, unknown>);
+  console.log(s);
+  console.log("---");
 }
