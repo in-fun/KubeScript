d3fa1ede03b39301a6b831a9825e02c60a753d42
lilac <cnjamesdeng@gmail.com>
2022-11-28 23:54:33 +0800

    Support config map

--- example/nginx/mod.ts
+++ example/nginx/mod.ts
@@ -1,7 +1,9 @@
 import deployment from "./deployment.ts";
 import service from "./service.ts";
+import configMap from './config-map.ts';
 
 export default [
   deployment,
   service,
+  configMap
 ];
---
+++ trait/gen-config-map.ts
@@ -0,0 +1,46 @@
+import {hash, url} from '../mod.ts';
+
+type DataType = {
+    [key: string]: string
+}
+
+/**
+ * Get an absolute path string relative to the base url.
+ * @param base a file url
+ * @param path a path relative to the url
+ * @returns 
+ */
+export function relativePath(base: string, path: string) {
+    return url.fileURLToPath(`${base}/../${path}`)
+}
+
+/**
+ * Read text files of a directory as an object.
+ * @param path the path to the config directory
+ * @returns 
+ */
+export function readConfigData(path: string | URL): DataType {
+    const map: DataType = Object.create({});
+    for (const dirEntry of Deno.readDirSync(path)) {
+        if (dirEntry.isDirectory) {
+            // skip nested directory for now.
+            continue;
+        }
+        const fileName = dirEntry.name;
+        const content = Deno.readTextFileSync(`${path}/${fileName}`)
+        map[fileName] =content
+    }
+    return map;
+}
+
+/**
+ * Generate a unique name using hash of the data.
+ * @param prefix the name prefix
+ * @param data 
+ * @param length the length of the auto genrated suffix. 
+ * @returns 
+ */
+export function generateName(prefix: string, data: DataType, length = 10): string {
+    const hex = hash(data);
+    return `${prefix}-${hex.slice(0, length)}`
+}
