d8867c2c42dda4062e29dec1f61a4c6c4343e2a0
Junjun Deng <cnjamesdeng@gmail.com>
2023-01-08 16:37:33 +0800

    Add test cases

---
+++ agent/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func Test_settings_parseManifests(t *testing.T) {
+	type fields struct {
+		repoPath string
+		paths    []string
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		manifest []*unstructured.Unstructured
+		revision string
+		wantErr  bool
+	}{
+		{
+			name: "Simple case",
+			fields: fields{
+				repoPath: "..",
+				paths:    []string{"../example/nginx"},
+			},
+		},
+		{
+			name: "Error when path is not ks dir",
+			fields: fields{
+				repoPath: "..",
+				paths:    []string{"../example"},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &settings{
+				repoPath: tt.fields.repoPath,
+				paths:    tt.fields.paths,
+			}
+			manifest, revision, err := s.parseManifests()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("settings.parseManifests() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err != nil {
+				t.Logf("Got error: %v", err)
+				return
+			}
+			if len(manifest) < 1 {
+				t.Errorf("No manifest returned")
+			}
+			if revision == "" {
+				t.Error("No non-empty revision returned")
+			}
+			t.Logf("Got manifest at revision %s:", revision)
+		})
+	}
+}
