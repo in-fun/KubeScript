904c5a4764b6c5006e320a6ea848a01a7e7d41c7
lilac <cnjamesdeng@gmail.com>
2023-02-11 00:02:49 +0800

    Example of tailscale deployment to okteto (#19)
    * Add tailscale deployment
    
    * Deploy with okteto
    
    * Use auth key to avoid manual authentication
    
    * Format files

---
+++ example/tailscale/deployment.ts
@@ -0,0 +1,47 @@
+import { args, Deployment, env } from "./deps.ts";
+import labels from "./labels.ts";
+
+const replicas = {
+  staging: 1,
+  production: 2,
+};
+
+const name = labels.app;
+const image = "ghcr.io/tailscale/tailscale:latest";
+
+const res: Deployment = {
+  apiVersion: "apps/v1",
+  kind: "Deployment",
+  metadata: {
+    name,
+    labels,
+  },
+  spec: {
+    selector: { matchLabels: labels },
+    replicas: replicas[env],
+    template: {
+      metadata: { labels },
+      spec: {
+        containers: [{
+          name: name,
+          image: image,
+          command: ["/bin/sh"],
+          args: [
+            "-c",
+            `tailscaled --tun=userspace-networking &
+             tailscale up --advertise-exit-node --auth-key ${args.key} & sleep infinity
+             `,
+          ],
+          env: [
+            {
+              name: "TS_USERSPACE",
+              value: "true",
+            },
+          ],
+        }],
+      },
+    },
+  },
+};
+
+export default res;
